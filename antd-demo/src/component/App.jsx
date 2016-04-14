1bfa975db4a43cd76ad5d047899aaa82d0c2bc5b
l1xnan <lixnan.g@gmail.com>
2016-04-14 22:20:36 +0800

    antd

---
+++ antd-demo/src/component/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+import { Button } from 'antd';
+import './App.less';
+
+const App = () =>
+  <DatePicker />;
+
+
+export default App;
+
---
+++ antd-demo/src/component/test.jsx
@@ -0,0 +1,4 @@
+React.createElement('div',null,
+React.createElement(MyLabel, {text:TextLabel}),
+React.createElement(MyTextfield,null),
+React.createElement(MyButton,{textlabel:"OK}))
